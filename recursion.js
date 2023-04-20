b06cfff935950d93ed06348cbe17fc838254c1ef
John <ashton.basile@gmail.com>
2023-04-20 11:43:15 +0100

    A better solution for the recursion problem

--- recursion.js
+++ recursion.js
@@ -15,9 +15,8 @@ way to fix this?
 function isEven(num){
 	if (num == 0) return true;
 	else if (num == 1) return false;
-	else if (num > 0) {
-		return isEven(num - 2);
-	} else return isEven(num + 2);
+	else if (num < 0) return isEven(-num);
+	else return isEven(num - 2);
 }
 console.log(isEven(50));
 console.log(isEven(75));
